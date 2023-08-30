e1c58c98d6de3762415b2fd1eb5d3dc73ac1820d
divaariani <putraputrikorea03@gmail.com>
2023-08-30 11:27:09 +0700

    update audit stock feature

--- lib/app/modules/views/audit_view.dart
+++ lib/app/modules/views/audit_view.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'scanaudit_view.dart';
+import 'home_view.dart';
 import '../../routes/app_pages.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: AuditView(),
+  ));
+}
 
 class AuditView extends StatefulWidget {
   const AuditView({Key? key}) : super(key: key);
@@ -30,46 +36,47 @@ class _AuditViewState extends State<AuditView> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        // Container(
-                        //   decoration: BoxDecoration(
-                        //     color: Colors.white,
-                        //     borderRadius: BorderRadius.circular(25),
-                        //   ),
-                        //   child: IconButton(
-                        //     onPressed: () {
-                        //       Navigator.push(
-                        //         context,
-                        //         MaterialPageRoute(
-                        //             builder: (context) => HomeView()),
-                        //       );
-                        //     },
-                        //     icon: Icon(Icons.arrow_back, color: Colors.black),
-                        //   ),
-                        // ),
-                        // SizedBox(width: 16),
-                        Expanded(
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Padding(
-                              padding: EdgeInsets.symmetric(vertical: 16),
-                              child: Text(
-                                "Audit Stock",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 16),
+                        child: Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                            border: Border.all(color: Colors.black),
+                          ),
+                          child: IconButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomeView()),
+                              );
+                            },
+                            icon: Icon(Icons.arrow_back, color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 16),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          child: Text(
+                            "Audit Stock",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
                             ),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                   SizedBox(height: 20),
                   Container(
@@ -89,49 +96,53 @@ class _AuditViewState extends State<AuditView> {
                       ),
                     ),
                   ),
-                  // SizedBox(height: 10),
+                  SizedBox(height: 10),
                   CardTable(),
                   SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ScanAuditView()),
-                          );
-                        },
-                        icon: Icon(Icons.qr_code_scanner),
-                        label: Text('Scan QR Code'),
-                        style: ElevatedButton.styleFrom(
-                          primary: const Color.fromRGBO(8, 77, 136, 136),
-                          onPrimary: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ScanAuditView()),
+                            );
+                          },
+                          icon: Icon(Icons.qr_code_scanner),
+                          label: Text('Scan QR Code'),
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromRGBO(8, 77, 136, 136),
+                            onPrimary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            elevation: 4,
+                            minimumSize: Size(160, 48),
                           ),
-                          elevation: 4,
-                          minimumSize: Size(160, 48),
                         ),
-                      ),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.pushNamed(context, Routes.HOME);
-                        },
-                        icon: Icon(Icons.exit_to_app),
-                        label: Text('Keluar'),
-                        style: ElevatedButton.styleFrom(
-                          primary: const Color.fromRGBO(8, 77, 136, 136),
-                          onPrimary: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0),
+                        SizedBox(width: 10), // Adjust the spacing here
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.pushNamed(context, Routes.HOME);
+                          },
+                          icon: Icon(Icons.exit_to_app),
+                          label: Text('Keluar'),
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromRGBO(8, 77, 136, 136),
+                            onPrimary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            elevation: 4,
+                            minimumSize: Size(120, 48),
                           ),
-                          elevation: 4,
-                          minimumSize: Size(160, 48),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -143,153 +154,159 @@ class _AuditViewState extends State<AuditView> {
   }
 }
 
+class MyData {
+  final String kode;
+  final String nama;
+  final String kuantitas;
+
+  MyData({
+    required this.kode,
+    required this.nama,
+    required this.kuantitas,
+  });
+}
+
+class MyDataTableSource extends DataTableSource {
+  final List<MyData> data;
+  MyDataTableSource(this.data);
+
+  @override
+  DataRow? getRow(int index) {
+    if (index >= data.length) {
+      return null;
+    }
+    final entry = data[index];
+    return DataRow.byIndex(
+      index: index,
+      cells: [
+        DataCell(Center(
+            child: Text(
+          entry.kode,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          ),
+        ))),
+        DataCell(Center(
+            child: Text(
+          entry.nama,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          ),
+        ))),
+        DataCell(Center(
+            child: Text(
+          entry.kuantitas,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          ),
+        ))),
+      ],
+    );
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => data.length;
+
+  @override
+  int get selectedRowCount => 0;
+}
+
 class CardTable extends StatelessWidget {
-  final TextStyle tableCellStyle = GoogleFonts.poppins(
-    fontWeight: FontWeight.w700,
-    color: Colors.white,
-    fontSize: 11,
-  );
-  final TextStyle tableCellnew = GoogleFonts.poppins(
-    fontWeight: FontWeight.bold,
-    color: Color.fromARGB(255, 3, 1, 49),
-    fontSize: 11,
-  );
+  final List<MyData> data = [
+    MyData(
+      kode: 'K10',
+      nama: 'Forklift',
+      kuantitas: 'C1',
+    ),
+    MyData(
+      kode: 'K10',
+      nama: 'Forklift',
+      kuantitas: 'C1',
+    ),
+    MyData(
+      kode: 'K10',
+      nama: 'Forklift',
+      kuantitas: 'C1',
+    ),
+    MyData(
+      kode: 'K10',
+      nama: 'Forklift',
+      kuantitas: 'C1',
+    ),
+    MyData(
+      kode: 'K10',
+      nama: 'Forklift',
+      kuantitas: 'C1',
+    ),
+    MyData(
+      kode: 'K10',
+      nama: 'Forklift',
+      kuantitas: 'C1',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: 20),
+        SizedBox(height: 10),
         Card(
-          margin: EdgeInsets.symmetric(horizontal: 20),
+          margin: EdgeInsets.symmetric(horizontal: 26),
           elevation: 4,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
-          color: Color.fromARGB(150, 255, 255, 255)!,
+          color: Color.fromARGB(255, 255, 255, 255)!,
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Audit Stock',
-                  style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    color: Color.fromARGB(255, 3, 1, 49),
-                    shadows: [
-                      Shadow(
-                        color: Colors.black.withOpacity(0.3),
-                        offset: Offset(0, 4),
-                        blurRadius: 3,
-                      ),
-                    ],
+            child: PaginatedDataTable(
+              header: Text(
+                'Audit Stock',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              columns: [
+                DataColumn(
+                    label: Text(
+                  'Kode',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal,
                   ),
-                ),
-                SizedBox(height: 10),
-                Table(
-                  border: TableBorder.all(color: Colors.black),
-                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                  children: [
-                    TableRow(children: [
-                      TableCell(
-                        child: Center(
-                          child: Text(
-                            'Kode Barang',
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.normal,
-                              color: Color.fromARGB(255, 3, 1, 49),
-                              fontSize: 12,
-                            ),
-                          ),
-                        ),
-                      ),
-                      TableCell(
-                        child: Center(
-                          child: Text(
-                            'Nama Barang',
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.normal,
-                              color: Color.fromARGB(255, 3, 1, 49),
-                              fontSize: 12,
-                            ),
-                          ),
-                        ),
-                      ),
-                      TableCell(
-                        child: Center(
-                          child: Text(
-                            'Kuantitas',
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.normal,
-                              color: Color.fromARGB(255, 3, 1, 49),
-                              fontSize: 12,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ]),
-                    TableRow(
-                      children: [
-                        TableCell(
-                          child:
-                              Center(child: Text('K10', style: tableCellnew)),
-                        ),
-                        TableCell(
-                          child: Center(
-                              child: Text('Forklift', style: tableCellnew)),
-                        ),
-                        TableCell(
-                          child: Center(child: Text('C1', style: tableCellnew)),
-                        ),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        TableCell(
-                          child:
-                              Center(child: Text('K10', style: tableCellnew)),
-                        ),
-                        TableCell(
-                          child: Center(
-                              child: Text('Forklift', style: tableCellnew)),
-                        ),
-                        TableCell(
-                          child: Center(child: Text('C1', style: tableCellnew)),
-                        ),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        TableCell(
-                          child:
-                              Center(child: Text('K10', style: tableCellnew)),
-                        ),
-                        TableCell(
-                          child: Center(
-                              child: Text('Forklift', style: tableCellnew)),
-                        ),
-                        TableCell(
-                          child: Center(child: Text('C1', style: tableCellnew)),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                )),
+                DataColumn(
+                    label: Text(
+                  'Nama Barang',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal,
+                  ),
+                )),
+                DataColumn(
+                    label: Text(
+                  'Kuantitas',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal,
+                  ),
+                )),
               ],
+              source: MyDataTableSource(data),
+              rowsPerPage: 5,
             ),
           ),
         ),
-        SizedBox(height: 20), 
       ],
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: AuditView(),
-  ));
 }
