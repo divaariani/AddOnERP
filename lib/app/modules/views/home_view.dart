6d3c02ef15f83abc1b9951ee915cde05f1ae9a33
divaariani <putraputrikorea03@gmail.com>
2023-09-05 09:49:45 +0700

    implement a login or authentication flow

--- lib/app/modules/views/home_view.dart
+++ lib/app/modules/views/home_view.dart
@@ -24,82 +24,90 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      bottomNavigationBar: CurvedNavigationBar(
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        
-        backgroundColor: Color(0xFFC5D7E4),
-        color: Color(0xFFC5D7E4),
-        animationDuration: Duration(milliseconds: 300),
-        items: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.home),
-              SizedBox(height: 4),
-              Text(
-                'Home',
-                style: TextStyle(fontSize: 12),
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeView()),
+        );
+        return false;
+      },
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          bottomNavigationBar: CurvedNavigationBar(
+            index: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            backgroundColor: Color(0xFFC5D7E4),
+            color: Color(0xFFC5D7E4),
+            animationDuration: Duration(milliseconds: 300),
+            items: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.home),
+                  SizedBox(height: 4),
+                  Text(
+                    'Home',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.trending_up),
+                  SizedBox(height: 4),
+                  Text(
+                    'Activity',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.qr_code_scanner),
+                  SizedBox(height: 4),
+                  Text(
+                    'QR Code',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.notifications),
+                  SizedBox(height: 4),
+                  Text(
+                    'Notifs',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.person),
+                  SizedBox(height: 4),
+                  Text(
+                    'Profile',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
               ),
             ],
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          body: Stack(
+            fit: StackFit.expand,
             children: [
-              Icon(Icons.trending_up),
-              SizedBox(height: 4),
-              Text(
-                'Activity',
-                style: TextStyle(fontSize: 12),
-              ),
+              _pages[_currentIndex],
             ],
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.qr_code_scanner),
-              SizedBox(height: 4),
-              Text(
-                'QR Code',
-                style: TextStyle(fontSize: 12),
-              ),
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.notifications),
-              SizedBox(height: 4),
-              Text(
-                'Notifs',
-                style: TextStyle(fontSize: 12),
-              ),
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.person),
-              SizedBox(height: 4),
-              Text(
-                'Profile',
-                style: TextStyle(fontSize: 12),
-              ),
-            ],
-          ),
-        ],
-      ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          _pages[_currentIndex],
-        ],
-      ),
-    );
+        ));
   }
 }
