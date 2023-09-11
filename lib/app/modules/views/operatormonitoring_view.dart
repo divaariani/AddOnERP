94434a1233ef568c4c9c510f419e3a10d9dfd6b8
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-09-11 15:34:49 +0700

    update webview for machine monitoring

--- lib/app/modules/views/operatormonitoring_view.dart
+++ lib/app/modules/views/operatormonitoring_view.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OperatorMonitorView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Monitoring'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(0.99, -0.14),
+              end: Alignment(-0.99, 0.14),
+              colors: [Color(0xFF2A77AC), Color(0xFF5AB4E1)],
+            ),
+          ),
+        ),
+        title: Text(
+          'Monitoring',
+          style: GoogleFonts.poppins(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        leading: InkWell(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Image.asset(
+            'assets/icon.back.png',
+          ),
+        ),
       ),
       body: WebView(
-        initialUrl: 'https://bierp.sutrakabel.com/#/monitoring-machine', 
+        initialUrl: 'https://bierp.sutrakabel.com/#/monitoring-machine',
         javascriptMode: JavascriptMode.unrestricted,
       ),
     );
