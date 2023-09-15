9d355bc5083e51ddcf497e8d4ac8b2b8c1d13913
Nur Zharifah Pradinda Kusuma <111584111+dindakusumaa@users.noreply.github.com>
2023-09-15 07:36:37 +0700

    Update sessionmanager in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 import 'app/widgets/splash.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'app/modules/utils/sessionmanager.dart';
+import 'app/modules/controllers/login_controller.dart';
+import 'app/modules/controllers/actor_controller.dart';
 
-void main() {
-  runApp(AddOnApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final sessionManager = SessionManager();
+  await sessionManager.initPrefs();
+  final isLoggedIn = await sessionManager.isLoggedIn();
+  Get.put(LoginController());
+  Get.put(ActorController()); 
+  runApp(AddOnApp(isLoggedIn: isLoggedIn));
 }
 
 class AddOnApp extends StatelessWidget {
+  final bool isLoggedIn;
+
+  AddOnApp({required this.isLoggedIn});
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
+    if (isLoggedIn) {
+      return FutureBuilder(
         future: Future.delayed(Duration(seconds: 2)),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -24,10 +37,22 @@ class AddOnApp extends StatelessWidget {
               theme: ThemeData(
                 textTheme: GoogleFonts.poppinsTextTheme(),
               ),
-              initialRoute: Routes.LOGIN,
+              initialRoute: Routes.HOME,
               getPages: AppPages.routes,
             );
           }
-        });
+        },
+      );
+    } else {
+      return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Sutrado Add On",
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(),
+        ),
+        initialRoute: Routes.LOGIN,
+        getPages: AppPages.routes,
+      );
+    }
   }
 }
