bf46b9d7864a0e7961bd856ede74567569f727af
Nur Zharifah Pradinda Kusuma <111584111+dindakusumaa@users.noreply.github.com>
2023-09-18 08:34:11 +0700

    Update button back gudangout_view

--- lib/app/modules/views/gudangout_view.dart
+++ lib/app/modules/views/gudangout_view.dart
@@ -21,7 +21,15 @@ class GudangOutView extends StatefulWidget {
 class _GudangOutViewState extends State<GudangOutView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+        return WillPopScope(
+      onWillPop: () async {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomeView()),
+        );
+        return false;
+      },
+    child: Scaffold(
       backgroundColor: Colors.transparent,
       body: Stack(
         fit: StackFit.expand,
@@ -182,6 +190,7 @@ class _GudangOutViewState extends State<GudangOutView> {
           ),
         ],
       ),
+    ),
     );
   }
 }
