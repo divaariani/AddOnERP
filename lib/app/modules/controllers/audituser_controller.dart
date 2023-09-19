8e5e91c18a5b83e1930272d19a7756ad56b05607
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-09-19 11:51:37 +0700

    update auditor controller

--- lib/app/modules/controllers/audituser_controller.dart
+++ lib/app/modules/controllers/audituser_controller.dart
@@ -1,23 +1,38 @@
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'dart:convert';
-import '../utils/globals.dart';
+import '../utils/sessionmanager.dart';
 
 class AuditUserController {
   static const String baseUrl = '{API}';
   late DateTime currentTime;
+  late String formattedDate;
+  final SessionManager sessionManager = SessionManager();
+  final SessionManager _sessionManager = SessionManager();
+  String userId = "";
+  String userName = "";
 
-  Future<void> _fetchCurrentTime() async {
-    try {
-      currentTime = DateTime.now();
-    } catch (error) {
-      print(error);
-    }
+  AuditUserController() {
+    _initialize();
   }
+
+  Future<void> _initialize() async {
+  try {
+    userId = await _sessionManager.getUserId() ?? "";
+    userName = await _sessionManager.getUsername() ?? "";
+    currentTime = DateTime.now(); 
+    formattedDate = DateFormat('ddMMyyyy').format(currentTime);
+    print(formattedDate); 
+  } catch (error) {
+    print(error);
+  }
+}
 
   Future<int> fetchIdInventory() async {
     try {
       final response = await http.get(
-        Uri.parse('$baseUrl?function=get_inventory_id&nama_auditor=$currentNameAuditor'),
+        Uri.parse(
+            '$baseUrl?function=get_inventory_id&nama_auditor=Auditor_$userName' + '_$formattedDate'),
       );
 
       if (response.statusCode == 200) {
@@ -40,6 +55,33 @@ class AuditUserController {
     } catch (error) {
       print(error);
       throw Exception('Failed to fetch idinventory from API');
+    }
+  }
+
+  Future<String> fetchNameInventory() async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+          '$baseUrl?function=get_inventory_id&nama_auditor=Auditor_$userName' + '_$formattedDate',
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body)['data'];
+
+        if (data.isNotEmpty) {
+          final String name = data.last['name'];
+          return name;
+        } else {
+          throw Exception('Data tidak ditemukan di API');
+        }
+      } else {
+        throw Exception(
+            'Gagal mendapatkan respon dari API: ${response.statusCode}');
+      }
+    } catch (error) {
+      print(error);
+      throw Exception('Gagal mengambil data name dari API');
     }
   }
 }
