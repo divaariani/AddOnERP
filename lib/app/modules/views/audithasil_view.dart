aee67794f6f94e7816c4ff5c1f5fd676b7b0e99a
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-09-19 09:27:18 +0700

    implement audit stock view logic

--- lib/app/modules/views/audithasil_view.dart
+++ lib/app/modules/views/audithasil_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'home_view.dart';
 import 'audit_view.dart';
+import '../controllers/auditview_controller.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,6 +17,14 @@ class AuditHasilView extends StatefulWidget {
 }
 
 class _AuditHasilViewState extends State<AuditHasilView> {
+  int page = 1;
+  int pageSize = 10;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -220,15 +230,17 @@ class _CustomButtonState extends State<CustomButton> {
 }
 
 class MyData {
+  final int id;
   final String lokasi;
   final String lotbarang;
-  final int kuantitas;
+  final int quantity;
   final String state;
 
   MyData({
+    required this.id,
     required this.lokasi,
     required this.lotbarang,
-    required this.kuantitas,
+    required this.quantity,
     required this.state,
   });
 }
@@ -246,39 +258,55 @@ class MyDataTableSource extends DataTableSource {
     return DataRow.byIndex(
       index: index,
       cells: [
-        DataCell(Center(
+        DataCell(
+          Container(
+            alignment: Alignment.centerLeft,
             child: Text(
-          entry.lokasi,
-          style: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold,
+              entry.lokasi,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ))),
-        DataCell(Center(
+        ),
+        DataCell(
+          Container(
+            alignment: Alignment.centerLeft,
             child: Text(
-          entry.lotbarang,
-          style: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold,
+              entry.lotbarang,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ))),
-        DataCell(Center(
+        ),
+        DataCell(
+          Container(
+            alignment: Alignment.centerLeft,
             child: Text(
-          entry.kuantitas.toString(),
-          style: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold,
+              '1',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ))),
-        DataCell(Center(
+        ),
+        DataCell(
+          Container(
+            alignment: Alignment.centerLeft,
             child: Text(
-          entry.state,
-          style: TextStyle(
-            fontSize: 12,
-            color: Colors.green,
-            fontWeight: FontWeight.bold,
+              'Confirm',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.green,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ))),
+        ),
       ],
     );
   }
@@ -293,17 +321,52 @@ class MyDataTableSource extends DataTableSource {
   int get selectedRowCount => 0;
 }
 
-class CardTable extends StatelessWidget {
-  final List<MyData> data = [
-    MyData(lokasi: 'IT', lotbarang: 'K0D2512', kuantitas: 1, state: 'Confirm'),
-    MyData(lokasi: 'IT', lotbarang: 'K0A0909', kuantitas: 1, state: 'Confirm'),
-    MyData(lokasi: 'HRD', lotbarang: 'K0H2512', kuantitas: 1, state: 'Confirm'),
-    MyData(lokasi: 'HRD', lotbarang: 'K0H2512', kuantitas: 1, state: 'Confirm'),
-    MyData(lokasi: 'HRD', lotbarang: 'K0H2512', kuantitas: 1, state: 'Confirm'),
-    MyData(lokasi: 'HRD', lotbarang: 'K0H2512', kuantitas: 1, state: 'Confirm'),
-    MyData(lokasi: 'IT', lotbarang: 'K0D2512', kuantitas: 1, state: 'Confirm'),
-    MyData(lokasi: 'IT', lotbarang: 'K0A0909', kuantitas: 1, state: 'Confirm'),
-  ];
+class CardTable extends StatefulWidget {
+  @override
+  _CardTableState createState() => _CardTableState();
+}
+
+class _CardTableState extends State<CardTable> {
+  List<MyData> _data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDataFromAPI();
+  }
+
+  Future<void> fetchDataFromAPI() async {
+    try {
+      final response = await AuditViewController.postFormData(
+        id: 1,
+        lokasi: '',
+        lotBarang: '',
+      );
+
+      final List<dynamic> nameDataList = response.data;
+      print('API Response: $nameDataList');
+
+      final List<MyData> myDataList = nameDataList.map((data) {
+        int id = int.tryParse(data['id'].toString()) ??0; 
+        String lokasi = data['lokasi'].toString(); 
+        String lotbarang = data['lot_barang'].toString();
+
+        return MyData(
+          id: id,
+          lokasi: lokasi,
+          lotbarang: lotbarang,
+          quantity: 1,
+          state: 'Confirm',
+        );
+      }).toList();
+
+      setState(() {
+        _data = myDataList;
+      });
+    } catch (e) {
+      print('Error fetching data: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -319,56 +382,93 @@ class CardTable extends StatelessWidget {
           ),
           color: Colors.white,
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: PaginatedDataTable(
-              header: Text(
-                'Audit Stock',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              columns: [
-                DataColumn(
-                    label: Text(
-                  'Lokasi',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.normal,
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: _data.isEmpty
+                ? EmptyData()
+                : PaginatedDataTable(
+                    columns: [
+                      DataColumn(
+                        label: Text(
+                          'Lokasi',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      DataColumn(
+                        label: Text(
+                          'Lot Barang',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      DataColumn(
+                        label: Text(
+                          'Kuantitas',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      DataColumn(
+                        label: Text(
+                          'Status',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                    source: MyDataTableSource(_data),
+                    rowsPerPage: 10,
                   ),
-                )),
-                DataColumn(
-                    label: Text(
-                  'Lot Barang',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.normal,
-                  ),
-                )),
-                DataColumn(
-                    label: Text(
-                  'Kuantitas',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.normal,
-                  ),
-                )),
-                DataColumn(
-                    label: Text(
-                  'Status',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.normal,
-                  ),
-                )),
-              ],
-              source: MyDataTableSource(data),
-              rowsPerPage: 10,
-            ),
           ),
         ),
       ],
+    );
+  }
+}
+
+class EmptyData extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 26),
+      child: Container(
+        width: 1 * MediaQuery.of(context).size.width,
+        height: 300,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icon.nodata.png',
+              width: 30,
+              height: 30,
+              color: Colors.blue[900],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Text(
+                'Tidak ada hasil scan barang',
+                style: GoogleFonts.poppins(
+                  color: Colors.blue[900],
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
