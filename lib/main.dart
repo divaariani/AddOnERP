1e15b56afd7b7ab87dc3b2121420a38520ddd5fe
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-10-06 13:40:31 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'app/routes/app_pages.dart';
 import 'app/widgets/splash.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'app/modules/utils/sessionmanager.dart';
 import 'app/modules/controllers/login_controller.dart';
 import 'app/modules/controllers/actor_controller.dart';
@@ -20,16 +20,16 @@ void main() async {
 class AddOnApp extends StatelessWidget {
   final bool isLoggedIn;
 
-  AddOnApp({required this.isLoggedIn});
+  const AddOnApp({Key? key, required this.isLoggedIn}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (isLoggedIn) {
       return FutureBuilder(
-        future: Future.delayed(Duration(seconds: 2)),
+        future: Future.delayed(const Duration(seconds: 2)),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return SplashScreen();
+            return const SplashScreen();
           } else {
             return GetMaterialApp(
               debugShowCheckedModeBanner: false,
