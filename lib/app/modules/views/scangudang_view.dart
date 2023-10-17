6f7f7d25f4f1d7c3e9be61a094af622199212f06
Nur Zharifah Pradinda Kusuma <111584111+dindakusumaa@users.noreply.github.com>
2023-10-17 10:42:38 +0700

    add const in scangudang_view

--- lib/app/modules/views/scangudang_view.dart
+++ lib/app/modules/views/scangudang_view.dart
@@ -1,9 +1,12 @@
+import 'package:addon/app/modules/views/gudanghasil_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-import 'gudang_view.dart';
+import '../utils/globals.dart';
+import 'gudanghasil_view.dart';
+import 'gudangmobil_view.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: ScanGudangView(),
   ));
 }
@@ -16,7 +19,7 @@ class ScanGudangView extends StatefulWidget {
 }
 
 class _ScanGudangViewState extends State<ScanGudangView> {
-  String _barcodeGudangResult = '';
+  String _barcodeGudangResult = 'Scan barcode pada mobil!';
 
   Future<void> _scanBarcode() async {
     String barcodeGudangResult = await FlutterBarcodeScanner.scanBarcode(
@@ -29,141 +32,140 @@ class _ScanGudangViewState extends State<ScanGudangView> {
     setState(() {
       _barcodeGudangResult = barcodeGudangResult;
     });
+
+    setGlobalBarcodeMobilResult(barcodeGudangResult);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GudangMobilView(result: barcodeGudangResult, resultBarangGudang: []),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => GudangView()),
-        );
-        return false;
-      },
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            Container(
-              width: 360,
-              height: 800,
-              clipBehavior: Clip.antiAlias,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Color(0xFF2A77AC), Color(0xFF5AB4E1)],
-                  stops: [0.6, 1.0],
-                ),
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            width: 360,
+            height: 800,
+            clipBehavior: Clip.antiAlias,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(0.99, -0.14),
+                end: Alignment(-0.99, 0.14),
+                colors: [Color(0xFF2A77AC), Color(0xFF5AB4E1)],
               ),
             ),
-            SingleChildScrollView(
-              child: SafeArea(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 70),
-                    Center(
-                      child: Padding(
-                        padding: EdgeInsets.all(40),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Color(0xFF084D88),
-                              width: 4,
+          ),
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(width: 10),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const GudangHasilView()),
+                          );
+                        },
+                        child: Image.asset('assets/icon.back.png',
+                            width: 60, height: 60),
+                      ),
+                      const SizedBox(width: 16),
+                      const Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          child: Text(
+                            "Scan Mobil",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
                             ),
-                            borderRadius: BorderRadius.circular(20),
                           ),
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 80),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image(
-                                  image: AssetImage("assets/icon.barcode.png"),
-                                ),
-                                SizedBox(height: 20),
-                                ElevatedButton(
-                                  onPressed: _scanBarcode,
-                                  style: ElevatedButton.styleFrom(
-                                    primary: Color(0xFF226EA4),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(40),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color:const Color(0xFF084D88),
+                            width: 4,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 80),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Image(
+                                image: AssetImage("assets/icon.barcode.png"),
+                              ),
+                              const SizedBox(height: 20),
+                              ElevatedButton(
+                                onPressed: _scanBarcode,
+                                style: ElevatedButton.styleFrom(
+                                  primary: const Color(0xFF226EA4),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
                                   ),
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Icon(
-                                        Icons.qr_code,
+                                ),
+                                child: const Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(
+                                      Icons.qr_code,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(width: 8),
+                                    Text(
+                                      "Scan QR Code",
+                                      style: TextStyle(
+                                        fontSize: 16,
                                         color: Colors.white,
                                       ),
-                                      SizedBox(width: 8),
-                                      Text(
-                                        "Scan QR Code",
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
                     ),
-                    Center(
-                      child: Text(
-                        "Scan barcode pada Barang!",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white,
-                        ),
-                      ),
+                  ),
+                  Text(
+                    textAlign: TextAlign.center, 
+                    "$_barcodeGudangResult",
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
                     ),
-                    SizedBox(height: 30),
-                  ],
-                ),
+                  ),
+                  SizedBox(height: 20),
+                ],
               ),
             ),
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: AppBar(
-                backgroundColor: Color(0xFF2A77AC),
-                elevation: 0.0,
-                leading: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => GudangView()),
-                    );
-                  },
-                  child: Image.asset(
-                    'assets/icon.back.png',
-                    width: 40,
-                    height: 40,
-                  ),
-                ),
-                title: Text(
-                  "Scan Barang",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
