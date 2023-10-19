4016d51a67bd2be558dca7a180f2319f83cb6f33
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-10-19 13:20:21 +0700

    add color theme

---
+++ lib/app/modules/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blueOne = Color(0xFF084D88);
+  static const Color blueTwo = Color(0xFF0171BB);
+  static const Color blueThree = Color(0xFF60BBE7);
+  static const Color white = Color(0xFFFAFAFA);
+  static const Color black = Color(0xFF19191B);
+  static const Color greyOne = Color(0xFF61646B);
+  static const Color greyTwo = Color(0xFFAFB1B6);
+  static const Color greyThree = Color(0xFFEFEFF0);
+}
