dc2a57f7d8bd6d0905c73ca2c634a6c5b8d4d0cb
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-11-01 09:02:17 +0700

    clean architecture notification controller

--- lib/app/modules/controllers/notification_controller.dart
+++ lib/app/modules/controllers/notification_controller.dart
@@ -1,9 +1,25 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'response_model.dart';
+import '../utils/sessionmanager.dart';
 
 class NotificationController{
   static const String baseUrl = '{API}';
+
+  final SessionManager _sessionManager = SessionManager();
+  String userId = "";
+
+  NotificationController() {
+    _initialize();
+  }
+
+  void _initialize() async {
+    try {
+      userId = await _sessionManager.getUserId() ?? "";
+    } catch (error) {
+      print(error);
+    }
+  }
 
   static Future<ResponseModel> postNotification({
     required int userid,
@@ -18,6 +34,34 @@ class NotificationController{
         'title': title,
         'description': description,
         'date': date.toString(),
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body); 
+      return ResponseModel.fromJson(responseData);
+    } else {
+      throw Exception('Failed to post form data');
+    }
+  }
+
+  Future<ResponseModel> viewData({
+    required int id,
+    required int userid,
+    required String title,
+    required String description,
+    required String date
+  }) async {
+    final userId = await _sessionManager.getUserId() ?? "";
+
+    final response = await http.post(
+      Uri.parse('$baseUrl?function=get_notification_activity&userid=$userId'),
+      body: {
+        'id': id.toString(),
+        'userid': userid.toString(),
+        'title': title,
+        'description': description,
+        'date': date
       },
     );
 
