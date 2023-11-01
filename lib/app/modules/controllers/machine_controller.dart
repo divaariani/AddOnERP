048baa6318148431e209e009cc29b2a02807ec1d
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-11-01 09:10:13 +0700

    clean architecture for machine controller

--- lib/app/modules/controllers/machine_controller.dart
+++ lib/app/modules/controllers/machine_controller.dart
@@ -5,7 +5,7 @@ import 'response_model.dart';
 class MachineController{
   static const String baseUrl = '{API}';
 
-  static Future<ResponseModel> postFormData({
+  static Future<ResponseModel> postFormOperator({
     required int id,
     required String name,
     required int userId,
@@ -20,6 +20,28 @@ class MachineController{
         'userid': userId.toString(),
         'namaoperator': namaoperator,
         'statusmesin': statusmesin,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body); 
+      return ResponseModel.fromJson(responseData);
+    } else {
+      throw Exception('Failed to post form data');
+    }
+  }
+
+  static Future<ResponseModel> postFormMachineState({
+    required int id,
+    required String state,
+    required String timestate,
+  }) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl?function=insert_machine_status'),
+      body: {
+        'pidworkcenter': id.toString(),
+        'pstatus': state,
+        'pstatustime': timestate,
       },
     );
 
